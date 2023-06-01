cffc2ecd9c6e380871016f4b2804dc680c9181cc
samueljuma <samueljuma1024@gmail.com>
2023-06-01 11:28:45 +0300

    added API_KEY & BASE URL to BuildConfig

--- app/src/main/java/com/samueljuma/movieapp/data/api/TMDBService.kt
+++ app/src/main/java/com/samueljuma/movieapp/data/api/TMDBService.kt
@@ -1,9 +1,13 @@
 package com.samueljuma.movieapp.data.api
 
+import com.samueljuma.movieapp.data.MovieList
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TMDBService {
 
+    @GET("movie/popular")
+    suspend fun getPopularMovies(@Query("api_key") apiKey:String): Response<MovieList>
 
 }
