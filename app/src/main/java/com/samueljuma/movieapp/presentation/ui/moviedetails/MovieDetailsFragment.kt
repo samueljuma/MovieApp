3563821523d977cab02f2a33da36a3a9a4a22a1e
samueljuma <samueljuma1024@gmail.com>
2023-06-08 15:01:49 +0300

    handled navigation to movie_details

--- app/src/main/java/com/samueljuma/movieapp/presentation/ui/moviedetails/MovieDetailsFragment.kt
+++ app/src/main/java/com/samueljuma/movieapp/presentation/ui/moviedetails/MovieDetailsFragment.kt
@@ -5,19 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.samueljuma.movieapp.R
+import com.samueljuma.movieapp.data.model.Movie
+import com.samueljuma.movieapp.databinding.FragmentMovieDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.WithFragmentBindings
 
 @AndroidEntryPoint
 class MovieDetailsFragment : Fragment() {
 
+    private lateinit var binding: FragmentMovieDetailsBinding
 
+    private val arguments: MovieDetailsFragmentArgs by navArgs()
+    private lateinit var selectedMovie: Movie
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+        binding = FragmentMovieDetailsBinding.inflate(layoutInflater, container, false)
+
+
+        selectedMovie = arguments.movie
+        binding.movie = selectedMovie
+        return binding.root
     }
 
 }
--- app/src/main/java/com/samueljuma/movieapp/presentation/ui/movielist/MovieListFragment.kt
+++ app/src/main/java/com/samueljuma/movieapp/presentation/ui/movielist/MovieListFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.samueljuma.movieapp.R
 import com.samueljuma.movieapp.databinding.FragmentMovieListBinding
@@ -39,6 +40,16 @@ class MovieListFragment : Fragment() {
 
         return binding.root
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.navigateToMovieDetails.observe(viewLifecycleOwner){movie->
+            movie?.let {
+                this.findNavController().navigate(MovieListFragmentDirections.actionMovieListFragmentToMovieDetailsFragment2(movie))
+                viewModel.doneNavigatingToMovieDetails()
+            }
+        }
     }
 
     private fun subscribeUI(adapter: MovieListAdapter, binding: FragmentMovieListBinding) {
