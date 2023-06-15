f7cda3e26f255e07350e2e8508d37591660cf375
samueljuma <samueljuma1024@gmail.com>
2023-06-15 08:56:19 +0300

    setup watchList UI

--- app/src/main/java/com/samueljuma/movieapp/presentation/ui/watchlist/WatchListFragment.kt
+++ app/src/main/java/com/samueljuma/movieapp/presentation/ui/watchlist/WatchListFragment.kt
@@ -1,4 +1,4 @@
-package com.samueljuma.movieapp.presentation.ui.moviedetails
+package com.samueljuma.movieapp.presentation.ui.watchlist
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,36 +10,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.MenuProvider
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
-import androidx.navigation.fragment.navArgs
+import androidx.navigation.fragment.findNavController
 import com.samueljuma.movieapp.R
-import com.samueljuma.movieapp.data.model.Movie
-import com.samueljuma.movieapp.databinding.FragmentMovieDetailsBinding
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.WithFragmentBindings
 
-@AndroidEntryPoint
-class MovieDetailsFragment : Fragment() {
+class WatchListFragment : Fragment() {
 
-    private lateinit var binding: FragmentMovieDetailsBinding
-
-    private val arguments: MovieDetailsFragmentArgs by navArgs()
-
-    private val viewModel: MovieDetailsViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentMovieDetailsBinding.inflate(layoutInflater, container, false)
-
-        viewModel.setMovie(arguments.movie)
-
-        viewModel.movie.observe(viewLifecycleOwner){movie ->
-            binding.movie = movie
-        }
-        return binding.root
+        return inflater.inflate(R.layout.fragment_watch_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -48,13 +30,13 @@ class MovieDetailsFragment : Fragment() {
          */
         val menuProvider = object : MenuProvider {
             override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.details_menu, menu)
+                menuInflater.inflate(R.menu.watch_list_menu, menu)
             }
 
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
                 return when(menuItem.itemId){
-                    R.id.menu_add_to_watchlist -> {
-                        Toast.makeText(context, "Add Under Construction", Toast.LENGTH_SHORT).show()
+                    R.id.menu_clear -> {
+                        Toast.makeText(context, "Clear Under Construction", Toast.LENGTH_SHORT).show()
                         true
                     }
 
@@ -70,5 +52,4 @@ class MovieDetailsFragment : Fragment() {
          * End of Menu handling
          */
     }
-
 }
